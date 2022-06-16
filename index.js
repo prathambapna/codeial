596c95ab428e9cba729073542e246b5d1e4ea43b
Pratham Bapna <manjubapna33@gmail.com>
2022-06-16 13:13:37 +0530

    layout.css added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port=8000;
 
 const expressLayouts=require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+
 //use it before using routes as it belongs to view which is going to  be rendered in routes so before it gets rendered we neet to specify layout
 app.use(expressLayouts);
 
