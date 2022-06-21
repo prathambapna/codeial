7b077b4c3389b5dbd9bbc2e21136458b783f017e
Pratham Bapna <manjubapna33@gmail.com>
2022-06-22 01:11:45 +0530

    displayed name of user posted it

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,24 @@
+const Post=require('../models/post');
 module.exports.home=function(req,res)
 {
-    //return res.end('<h1>Express is up for Codeial!</h1>')
-    return res.render('home',{
-        title:'Home'
-    });
+
+    //with this if we do post.user in home.ejs it only gives id of the user,but if we need name,email,timecreated we need to populate the user
+    
+    // Post.find({},function(err,posts)
+    // {
+    //     return res.render('home',{
+    //         title:'Codeial | Home',
+    //         posts:posts
+    //     });
+    // })
+
+
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts)
+    {
+        return res.render('home',{
+            title:'Codeial | Home',
+            posts:posts
+        });
+    })
 }
