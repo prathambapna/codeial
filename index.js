db5dd36f6a4325a32576c2231510c1ce2fd82796
Pratham Bapna <manjubapna33@gmail.com>
2022-06-21 18:02:46 +0530

    sass middleware included

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ const passportLocal=require('./config/passport-local-strategy');
 const MongoStore= require('connect-mongo');
 
 
+// setting up sass
+const sassMiddleware=require('node-sass-middleware');
+app.use(sassMiddleware({
+    src:'/assets/scss',
+    dest:'/assets/css',
+    debug:true,
+    outputStyle:'extended',
+    prefix:'/css'
+}))
+
 
 //used for params and query in post request
 app.use(express.urlencoded());
