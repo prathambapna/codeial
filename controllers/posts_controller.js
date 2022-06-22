fc561009ac5313f57eb00c7572fd85e3729eb3be
Pratham Bapna <manjubapna33@gmail.com>
2022-06-22 16:28:46 +0530

    button to delete posts with authorization and managing database

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post=require('../models/post');
+const Comment=require('../models/comment');
 module.exports.create=function(req,res)
 {
     Post.create({
@@ -12,4 +13,23 @@ module.exports.create=function(req,res)
         if(err){console.log('error in creating post');return;}
         return res.redirect('back');
     })
+}
+
+//will be deleting according to action=> /posts/destroy/:id
+//in params there would be id of post
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        //.id means converting the object id (._id) to string
+        if(post.user==req.user.id)
+        {
+            post.remove();
+            Comment.deleteMany({
+                post:req.params.id
+            },function(err){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    });
 }
